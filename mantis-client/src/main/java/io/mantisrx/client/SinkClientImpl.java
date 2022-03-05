f176032b29ca9e57806e7a02f53dafded8394beb
Calvin Cheung <ccheung@netflix.com>
2022-03-04 17:47:56 -0800

    Only count resubmission from worker failures

--- mantis-client/src/main/java/io/mantisrx/client/SinkClientImpl.java
+++ mantis-client/src/main/java/io/mantisrx/client/SinkClientImpl.java
@@ -27,6 +27,7 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
+import lombok.ToString;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import rx.Observable;
