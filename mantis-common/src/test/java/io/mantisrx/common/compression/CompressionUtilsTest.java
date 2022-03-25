ac33102e4de13c2840d3e7a0ffb165de9b547c74
Calvin Cheung <ccheung@netflix.com>
2022-03-25 16:21:22 -0700

    spotless

--- mantis-common/src/test/java/io/mantisrx/common/compression/CompressionUtilsTest.java
+++ mantis-common/src/test/java/io/mantisrx/common/compression/CompressionUtilsTest.java
@@ -24,8 +24,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import org.junit.Assert;
 import org.junit.Test;
 
 public class CompressionUtilsTest {
