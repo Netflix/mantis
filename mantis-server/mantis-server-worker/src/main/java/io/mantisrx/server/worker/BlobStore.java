d6c6d8e30a05efedde40ed44a5e52d51e688f552
Sundaram Ananthanarayanan <sananthanarayanan@netflix.com>
2022-03-28 15:20:12 -0700

    [Part 6]: Blob Store abstractions and a class loader handle that can use it
    Two features are added through this change.
    1). Firstly, I'm introducing a new blob store abstraction and a few implementations that can help download the blob from various stores such as s3, artifactory, etc...
    2). A new class loader implementation has also been added that can use the blob store to download the requested artifacts from s3 or artifactory or elsewhere and create a class loader object that can run the user's application code.

---
+++ mantis-server/mantis-server-worker/src/main/java/io/mantisrx/server/worker/BlobStore.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2022 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.mantisrx.server.worker;
+
+import java.io.Closeable;
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import net.lingala.zip4j.ZipFile;
+import org.apache.commons.io.FilenameUtils;
+
+/**
+ * Abstraction to deal with getting files stored in object stores such as s3.
+ */
+public interface BlobStore extends Closeable {
+    File get(URI blobUrl) throws IOException;
+
+    /**
+     * blob store that adds a prefix to every requested URI.
+     *
+     * @param prefixUri prefix that needs to be prepended to every requested resource.
+     * @return blob store with the prefix patterns baked in.
+     */
+    default BlobStore withPrefix(URI prefixUri) {
+        return new PrefixedBlobStore(prefixUri, this);
+    }
+
+    /**
+     * blob store that when downloading zip files, also unpacks them and returns the unpacked file/directory to the caller.
+     *
+     * @return blob store that can effectively deal with zip files
+     */
+    default BlobStore withZipCapabilities() {
+        return new ZipHandlingBlobStore(this);
+    }
+
+    @RequiredArgsConstructor(access = AccessLevel.PACKAGE)
+    class PrefixedBlobStore implements BlobStore {
+        private final URI rootUri;
+        private final BlobStore blobStore;
+
+        @Override
+        public File get(URI blobUrl) throws IOException {
+            final String fileName = FilenameUtils.getName(blobUrl.toString());
+            return blobStore.get(rootUri.resolve(fileName));
+        }
+
+        @Override
+        public void close() throws IOException {
+            blobStore.close();
+        }
+    }
+
+    @RequiredArgsConstructor(access = AccessLevel.PACKAGE)
+    class ZipHandlingBlobStore implements BlobStore {
+
+        private final BlobStore blobStore;
+
+        @Override
+        public File get(URI blobUrl) throws IOException {
+            final File localFile = blobStore.get(blobUrl);
+            final ZipFile zipFile = isZipFile(localFile);
+            if (zipFile == null) {
+                return localFile;
+            } else {
+                String destDirStr = getDestDir(zipFile);
+                File destDir = new File(destDirStr);
+                if (!destDir.exists()) {
+                    zipFile.extractAll(destDirStr);
+                }
+                return destDir;
+            }
+        }
+
+        @Override
+        public void close() throws IOException {
+            blobStore.close();
+        }
+
+        private String getDestDir(ZipFile zipFile) {
+            return zipFile.getFile().getPath() + "-unzipped";
+        }
+
+        private ZipFile isZipFile(File file) {
+            ZipFile file1 = new ZipFile(file);
+            if (file1.isValidZipFile()) {
+                return file1;
+            } else {
+                return null;
+            }
+        }
+    }
+
+
+}
---
+++ mantis-server/mantis-server-worker/src/test/java/io/mantisrx/server/worker/TestBlobStore.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2022 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.mantisrx.server.worker;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import io.mantisrx.server.worker.BlobStore.PrefixedBlobStore;
+import java.io.File;
+import java.net.URI;
+import org.junit.Test;
+import org.mockito.Matchers;
+
+public class TestBlobStore {
+  @Test
+  public void testPrefixedBlobStore() throws Exception {
+    final BlobStore blobStore = mock(BlobStore.class);
+    final File file = mock(File.class);
+    when(blobStore.get(any())).thenReturn(file);
+
+    final BlobStore prefixedBlobStpre =
+        new PrefixedBlobStore(new URI("s3://netflix.s3.genpop.prod/mantis/jobs/"), blobStore);
+    prefixedBlobStpre.get(new URI("http://sananthanarayanan-mantis-jobs-sine-function-thin-0.1.0.zip"));
+
+    final URI expectedUri =
+        new URI("s3://netflix.s3.genpop.prod/mantis/jobs/sananthanarayanan-mantis-jobs-sine-function-thin-0.1.0.zip");
+    verify(blobStore, times(1)).get(Matchers.eq(expectedUri));
+
+    prefixedBlobStpre.get(new URI("https://mantis.us-east-1.prod.netflix.net/mantis-artifacts/sananthanarayanan-mantis-jobs-sine-function-thin-0.1.0.zip"));
+    verify(blobStore, times(2)).get(Matchers.eq(expectedUri));
+  }
+}
