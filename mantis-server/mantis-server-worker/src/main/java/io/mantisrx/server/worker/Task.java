fb9e6da8b53b53f081632828f1a1d9a3168ca2bd
sundargates <me@sundaram.io>
2022-03-28 22:35:53 -0700

    Indent task using 4 spaces (#156)
    Refactoring the code using 4 spaces for indent
    
    Co-authored-by: Sundaram Ananthanarayanan <sananthanarayanan@netflix.com>

--- mantis-server/mantis-server-worker/src/main/java/io/mantisrx/server/worker/Task.java
+++ mantis-server/mantis-server-worker/src/main/java/io/mantisrx/server/worker/Task.java
@@ -18,6 +18,7 @@ package io.mantisrx.server.worker;
 import io.mantisrx.server.core.ExecuteStageRequest;
 import io.mantisrx.server.core.Service;
 import io.mantisrx.server.core.Status;
+import io.mantisrx.server.core.domain.WorkerId;
 import io.mantisrx.server.core.metrics.MetricsFactory;
 import io.mantisrx.server.master.client.MantisMasterGateway;
 import io.mantisrx.server.worker.client.WorkerMetricsClient;
@@ -39,95 +40,99 @@ import rx.subjects.PublishSubject;
 @RequiredArgsConstructor
 public class Task extends AbstractIdleService {
 
-  private final ExecuteStageRequest executeStageRequest;
+    private final ExecuteStageRequest executeStageRequest;
 
-  private final WorkerConfiguration config;
+    private final WorkerConfiguration config;
 
-  private final List<Service> mantisServices = new ArrayList<>();
+    private final List<Service> mantisServices = new ArrayList<>();
 
-  private final MantisMasterGateway masterMonitor;
+    private final MantisMasterGateway masterMonitor;
 
-  /**
-   * The classpaths used by this task.
-   */
-  private final Collection<URL> requiredClasspaths;
+    /**
+     * The classpaths used by this task.
+     */
+    private final Collection<URL> requiredClasspaths;
 
-  private final ClassLoaderHandle classLoaderHandle;
+    private final ClassLoaderHandle classLoaderHandle;
 
-  private final SinkSubscriptionStateHandler.Factory sinkSubscriptionStateHandlerFactory;
+    private final SinkSubscriptionStateHandler.Factory sinkSubscriptionStateHandlerFactory;
 
-  private final PublishSubject<Observable<Status>> tasksStatusSubject = PublishSubject.create();
+    private final PublishSubject<Observable<Status>> tasksStatusSubject = PublishSubject.create();
 
-  @Override
-  public void startUp() {
-    try {
-      log.info("Starting current task {}", this);
-      doRun();
-    } catch (Exception e) {
-      log.error("Failed executing the task {}", executeStageRequest, e);
-    }
-  }
-
-  public void doRun() throws Exception {
-    // shared state
-    PublishSubject<WrappedExecuteStageRequest> executeStageSubject = PublishSubject.create();
-    PublishSubject<VirtualMachineTaskStatus> vmTaskStatusSubject = PublishSubject.create();
-
-    mantisServices.add(MetricsFactory.newMetricsServer(config, executeStageRequest));
-    mantisServices.add(MetricsFactory.newMetricsPublisher(config, executeStageRequest));
-    WorkerMetricsClient workerMetricsClient = new WorkerMetricsClient(masterMonitor);
-
-    mantisServices.add(new ExecuteStageRequestService(
-        executeStageSubject,
-        tasksStatusSubject,
-        new WorkerExecutionOperationsNetworkStage(
-            vmTaskStatusSubject,
-            masterMonitor,
-            config,
-            workerMetricsClient, sinkSubscriptionStateHandlerFactory),
-        getJobProviderClass(), classLoaderHandle, requiredClasspaths, null));
-
-    log.info("Starting Mantis Worker for task {}", this);
-    for (Service service : mantisServices) {
-      log.info("Starting service: " + service.getClass().getName());
-      try {
-        service.start();
-      } catch (Throwable e) {
-        log.error(String.format("Failed to start service %s: %s", service, e.getMessage()), e);
-        throw e;
-      }
+    @Override
+    public void startUp() {
+        try {
+            log.info("Starting current task {}", this);
+            doRun();
+        } catch (Exception e) {
+            log.error("Failed executing the task {}", executeStageRequest, e);
+        }
     }
 
-    // now that all the services have been started, let's submit the request
-    executeStageSubject.onNext(new WrappedExecuteStageRequest(PublishSubject.create(), executeStageRequest));
-  }
+    public void doRun() throws Exception {
+        // shared state
+        PublishSubject<WrappedExecuteStageRequest> executeStageSubject = PublishSubject.create();
+        PublishSubject<VirtualMachineTaskStatus> vmTaskStatusSubject = PublishSubject.create();
 
-  @Override
-  public void shutDown() {
-    log.info("Attempting to cancel task {}", this);
-    for (Service service : mantisServices) {
-      log.info("Stopping service: " + service.getClass().getName());
-      try {
-        service.shutdown();
-      } catch (Throwable e) {
-        log.error(String.format("Failed to stop service %s: %s", service, e.getMessage()), e);
-        throw e;
-      }
+        mantisServices.add(MetricsFactory.newMetricsServer(config, executeStageRequest));
+        mantisServices.add(MetricsFactory.newMetricsPublisher(config, executeStageRequest));
+        WorkerMetricsClient workerMetricsClient = new WorkerMetricsClient(masterMonitor);
+
+        mantisServices.add(new ExecuteStageRequestService(
+            executeStageSubject,
+            tasksStatusSubject,
+            new WorkerExecutionOperationsNetworkStage(
+                vmTaskStatusSubject,
+                masterMonitor,
+                config,
+                workerMetricsClient, sinkSubscriptionStateHandlerFactory),
+            getJobProviderClass(), classLoaderHandle, requiredClasspaths, null));
+
+        log.info("Starting Mantis Worker for task {}", this);
+        for (Service service : mantisServices) {
+            log.info("Starting service: " + service.getClass().getName());
+            try {
+                service.start();
+            } catch (Throwable e) {
+                log.error(String.format("Failed to start service %s: %s", service, e.getMessage()), e);
+                throw e;
+            }
+        }
+
+        // now that all the services have been started, let's submit the request
+        executeStageSubject.onNext(new WrappedExecuteStageRequest(PublishSubject.create(), executeStageRequest));
     }
-  }
 
-  private Optional<String> getJobProviderClass() {
-    io.mantisrx.shaded.com.google.common.base.Optional<String> jobProviderClass =
-        executeStageRequest.getNameOfJobProviderClass();
-    if (jobProviderClass.isPresent()) {
-      return Optional.of(executeStageRequest.getNameOfJobProviderClass().get());
-    } else {
-      return Optional.empty();
+    @Override
+    public void shutDown() {
+        log.info("Attempting to cancel task {}", this);
+        for (Service service : mantisServices) {
+            log.info("Stopping service: " + service.getClass().getName());
+            try {
+                service.shutdown();
+            } catch (Throwable e) {
+                log.error(String.format("Failed to stop service %s: %s", service, e.getMessage()), e);
+                throw e;
+            }
+        }
     }
-  }
 
-  public Observable<Status> getStatus() {
-    return tasksStatusSubject
-        .flatMap((Func1<Observable<Status>, Observable<Status>>) status -> status);
-  }
+    private Optional<String> getJobProviderClass() {
+        io.mantisrx.shaded.com.google.common.base.Optional<String> jobProviderClass =
+            executeStageRequest.getNameOfJobProviderClass();
+        if (jobProviderClass.isPresent()) {
+            return Optional.of(executeStageRequest.getNameOfJobProviderClass().get());
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public Observable<Status> getStatus() {
+        return tasksStatusSubject
+            .flatMap((Func1<Observable<Status>, Observable<Status>>) status -> status);
+    }
+
+    public WorkerId getWorkerId() {
+        return executeStageRequest.getWorkerId();
+    }
 }
