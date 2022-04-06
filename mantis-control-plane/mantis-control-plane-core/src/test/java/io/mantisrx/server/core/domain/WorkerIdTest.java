425caa6bf0b38aad2a5a14d78908dbbcba8e8b49
sundargates <me@sundaram.io>
2022-04-06 14:22:05 -0700

    Making WorkerId serializable by both jackson and Java (#165)
    Co-authored-by: Sundaram Ananthanarayanan <sananthanarayanan@netflix.com>

---
+++ mantis-control-plane/mantis-control-plane-core/src/test/java/io/mantisrx/server/core/domain/WorkerIdTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2022 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.mantisrx.server.core.domain;
+
+import static org.junit.Assert.assertEquals;
+
+import io.mantisrx.common.JsonSerializer;
+import org.apache.commons.lang3.SerializationUtils;
+import org.junit.Test;
+
+public class WorkerIdTest {
+    private final JsonSerializer serializer = new JsonSerializer();
+
+    @Test
+    public void testSerializationAndDeserialization() throws Exception {
+        WorkerId workerId = WorkerId.fromId("late-sine-function-tutorial-1-worker-0-1").get();
+        String s = serializer.toJson(workerId);
+        assertEquals(s, "{\"jobCluster\":\"late-sine-function-tutorial\",\"jobId\":\"late-sine-function-tutorial-1\",\"workerIndex\":0,\"workerNum\":1}");
+
+        WorkerId actual = serializer.fromJSON(s, WorkerId.class);
+        assertEquals(workerId, actual);
+    }
+
+    @Test
+    public void testIfWorkerIdIsSerializableUsingJava() throws Exception {
+        WorkerId workerId = WorkerId.fromId("late-sine-function-tutorial-1-worker-0-1").get();
+        byte[] serialized = SerializationUtils.serialize(workerId);
+        assertEquals(workerId, SerializationUtils.deserialize(serialized));
+    }
+}
