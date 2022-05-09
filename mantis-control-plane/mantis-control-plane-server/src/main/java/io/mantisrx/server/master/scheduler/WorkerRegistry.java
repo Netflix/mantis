a4f2048257b45afc32e57e3b8ef0c7af5999a204
Sundaram Ananthanarayanan <sananthanarayanan@netflix.com>
2022-05-09 15:57:08 -0700

    Added documentation

--- mantis-control-plane/mantis-control-plane-server/src/main/java/io/mantisrx/server/master/scheduler/WorkerRegistry.java
+++ mantis-control-plane/mantis-control-plane-server/src/main/java/io/mantisrx/server/master/scheduler/WorkerRegistry.java
@@ -26,7 +26,12 @@ import javax.annotation.Nullable;
 
 public interface WorkerRegistry {
 
-    /* Returns number of workers in LAUNCHED, START_INITIATED and STARTED state */
+    /**
+     * get the number of workers that are running in a given cluster.
+     * @param clusterID optional parameter to filter only workers for a given cluster.
+     *                  If null, then workers without a clusterID are filtered.
+     * @return number of workers in LAUNCHED, START_INITIATED and STARTED state
+     */
     int getNumRunningWorkers(@Nullable ClusterID clusterID);
 
 
