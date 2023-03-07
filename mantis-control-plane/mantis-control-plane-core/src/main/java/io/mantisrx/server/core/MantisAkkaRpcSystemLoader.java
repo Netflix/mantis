3c9e649b71cab2bd2c4e94df2ad53f24c42082bd
fdc-ntflx <103213338+fdc-ntflx@users.noreply.github.com>
2023-03-06 22:17:15 -0800

    Add flink-rpc-akka custom classloader (#352)

--- mantis-control-plane/mantis-control-plane-core/src/main/java/io/mantisrx/server/core/MantisAkkaRpcSystemLoader.java
+++ mantis-control-plane/mantis-control-plane-core/src/main/java/io/mantisrx/server/core/MantisAkkaRpcSystemLoader.java
@@ -16,10 +16,20 @@
 
 package io.mantisrx.server.core;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ServiceLoader;
+import java.util.UUID;
 import org.apache.flink.configuration.Configuration;
+import org.apache.flink.configuration.CoreOptions;
+import org.apache.flink.core.classloading.ComponentClassLoader;
 import org.apache.flink.runtime.rpc.RpcSystem;
 import org.apache.flink.runtime.rpc.RpcSystemLoader;
-import org.apache.flink.runtime.rpc.akka.AkkaRpcSystem;
+import org.apache.flink.util.IOUtils;
 
 /**
  * RpcSystemLoader for mantis task executor and other services that need to expose an RPC API.
@@ -27,13 +37,62 @@ import org.apache.flink.runtime.rpc.akka.AkkaRpcSystem;
  */
 public class MantisAkkaRpcSystemLoader implements RpcSystemLoader {
 
-    private static final RpcSystem INSTANCE = new AkkaRpcSystem();
+    private static final RpcSystem INSTANCE = createRpcSystem();
+
+    public static RpcSystem getInstance() {
+        return INSTANCE;
+    }
+
     @Override
     public RpcSystem loadRpcSystem(Configuration config) {
         return INSTANCE;
     }
 
-    public static RpcSystem getInstance() {
-        return INSTANCE;
+    private static RpcSystem createRpcSystem() {
+        try {
+            final ClassLoader flinkClassLoader = RpcSystem.class.getClassLoader();
+
+            final Path tmpDirectory = Paths.get(System.getProperty("java.io.tmpdir"));
+            Files.createDirectories(tmpDirectory);
+            final Path tempFile =
+                Files.createFile(
+                    tmpDirectory.resolve("flink-rpc-akka_" + UUID.randomUUID() + ".jar"));
+
+            final InputStream resourceStream =
+                flinkClassLoader.getResourceAsStream("flink-rpc-akka.jar");
+            if (resourceStream == null) {
+                throw new RuntimeException(
+                    "Akka RPC system could not be found. If this happened while running a test in the IDE,"
+                        + "run the process-resources phase on flink-rpc/flink-rpc-akka-loader via maven.");
+            }
+
+            IOUtils.copyBytes(resourceStream, Files.newOutputStream(tempFile));
+
+            // The following classloader loads all classes from the submodule jar, except for explicitly white-listed packages.
+            //
+            //  <p>To ensure that classes from the submodule are always loaded through the submodule classloader
+            //  (and thus from the submodule jar), even if the classes are also on the classpath (e.g., during
+            //  tests), all classes from the "org.apache.flink" package are loaded child-first.
+            //
+            //  <p>Classes related to mantis-specific or logging are loaded parent-first.
+            //
+            //  <p>All other classes can only be loaded if they are either available in the submodule jar or the
+            //  bootstrap/app classloader (i.e., provided by the JDK).
+            final ComponentClassLoader componentClassLoader = new ComponentClassLoader(
+                new URL[] {tempFile.toUri().toURL()},
+                flinkClassLoader,
+                // Dependencies which are needed by logic inside flink-rpc-akka (ie AkkaRpcService/System) which
+                // are external to flink-rpc-akka itself (like ExecuteStageRequest in mantis-control-plane).
+                CoreOptions.parseParentFirstLoaderPatterns(
+                    "org.slf4j;org.apache.log4j;org.apache.logging;org.apache.commons.logging;ch.qos.logback;io.mantisrx.server.worker;io.mantisrx.server.core;io.mantisrx", ""),
+                new String[] {"org.apache.flink"});
+
+            return new CleanupOnCloseRpcSystem(
+                ServiceLoader.load(RpcSystem.class, componentClassLoader).iterator().next(),
+                componentClassLoader,
+                tempFile);
+        } catch (IOException e) {
+            throw new RuntimeException("Could not initialize RPC system.", e);
+        }
     }
 }
