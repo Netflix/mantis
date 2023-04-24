07ad13495b36c32f60850afc7b24e801fbe64a2e
Salma <salma.afifi5555@gmail.com>
2023-04-24 13:12:47 +0200

    Fixup4: fixed issues in HeartbeatTest

--- mantis-server/mantis-server-worker/src/test/java/io/mantisrx/server/worker/HeartbeatTest.java
+++ mantis-server/mantis-server-worker/src/test/java/io/mantisrx/server/worker/HeartbeatTest.java
@@ -22,9 +22,11 @@ import io.mantisrx.server.core.Status;
 import io.mantisrx.server.core.StatusPayloads;
 import java.util.List;
 import org.junit.Test;
+import org.slf4j.Logger;
 
 
 public class HeartbeatTest {
+    private static final Logger logger = org.slf4j.LoggerFactory.getLogger(HeartbeatTest.class);
 
     @Test
     public void testSingleUsePayloads() {
@@ -34,16 +36,20 @@ public class HeartbeatTest {
         int val2 = 12;
         heartbeat.addSingleUsePayload("" + StatusPayloads.Type.IncomingDataDrop, "" + val1);
         heartbeat.addSingleUsePayload("" + StatusPayloads.Type.IncomingDataDrop, "" + val2);
-        final Status currentHeartbeatStatus = heartbeat.getCurrentHeartbeatStatus();
-        List<Status.Payload> payloads = currentHeartbeatStatus.getPayloads();
+        List<Status.Payload> payloads = heartbeat.getCurrentHeartbeatStatus().getPayloads();
+
+        logger.debug("Current Payloads: {}", payloads);
         assertEquals(2, payloads.size());
+
         int value = 0;
         for (Status.Payload p : payloads) {
             if (StatusPayloads.Type.valueOf(p.getType()) == StatusPayloads.Type.IncomingDataDrop)
                 value = Integer.parseInt(p.getData());
         }
         assertEquals(val2, value);
+
         payloads = heartbeat.getCurrentHeartbeatStatus().getPayloads();
+        logger.debug("Payloads after draining single-use payloads: {}", payloads);
         assertEquals(1, payloads.size());
     }
 }
