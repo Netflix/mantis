6422549055b11becf36f5ec3c019ba2a7267b99a
Andy Zhang <andyz@netflix.com>
2023-07-09 22:25:05 -0700

    basic submit

--- mantis-testcontainers/src/test/java/TestContainerHelloWorld.java
+++ mantis-testcontainers/src/test/java/TestContainerHelloWorld.java
@@ -23,15 +23,16 @@ import io.mantisrx.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 import io.mantisrx.shaded.org.apache.curator.framework.CuratorFramework;
 import io.mantisrx.shaded.org.apache.curator.framework.CuratorFrameworkFactory;
 import io.mantisrx.shaded.org.apache.curator.retry.RetryOneTime;
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.HttpUrl;
 import okhttp3.HttpUrl.Builder;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 import org.junit.Test;
 import org.testcontainers.containers.GenericContainer;
@@ -43,11 +44,36 @@ public class TestContainerHelloWorld {
     private static final int CONTROL_PLANE_API_PORT = 8100;
     private static final int CONTROL_PLANE_API_V2_PORT = 8075;
 
+    private static final int JOB_SINK_PORT = 5055;
+
     private static final String ZOOKEEPER_ALIAS = "zookeeper";
 
     private static final String CONTROL_PLANE_ALIAS = "mantiscontrolplane";
 
     private static final String CLUSTER_ID = "testcluster1";
+
+    private static final String JOB_CLUSTER_NAME = "hello-sine-testcontainers";
+
+    private static final String JOB_CLUSTER_CREATE = "{\"jobDefinition\":{\"name\":\"hello-sine-testcontainers\","
+        + "\"user\":\"mantisoss\",\"jobJarFileLocation\":\"file:///mantis-examples-sine-function-2.1.0-SNAPSHOT"
+        + ".zip\"," +
+        "\"version\":\"0.2.9 2018-05-29 16:12:56\",\"schedulingInfo\":{\"stages\":{" +
+        "\"1\":{\"numberOfInstances\":\"1\",\"machineDefinition\":{\"cpuCores\":\"1\",\"memoryMB\":\"1024\",\"diskMB\":\"1024\",\"networkMbps\":\"128\",\"numPorts\":\"1\"},\"scalable\":true," +
+        "\"scalingPolicy\":{" +
+        "},\"softConstraints\":[],\"hardConstraints\":[]}}}," +
+        "\"parameters\":[{\"name\":\"useRandom\",\"value\":\"false\"}],\"labels\":[{\"name\":\"_mantis"
+        + ".resourceCluster\",\"value\":\"testcluster1\"},"
+        + "{\"name\":\"_mantis.user\",\"value\":\"mantisoss\"},{\"name\":\"_mantis"
+        + ".ownerEmail\",\"value\":\"mantisoss@netflix.com\"},{\"name\":\"_mantis.jobType\",\"value\":\"other\"},"
+        + "{\"name\":\"_mantis.criticality\",\"value\":\"low\"},{\"name\":\"_mantis.artifact.version\",\"value\":\"0.2.9\"}]," +
+        "\"migrationConfig\":{\"strategy\":\"PERCENTAGE\",\"configString\":\"{\\\"percentToMove\\\":25,\\\"intervalMs\\\":60000}\"},\"slaMin\":\"0\",\"slaMax\":\"0\",\"cronSpec\":null,\"cronPolicy\":\"KEEP_EXISTING\",\"isReadyForJobMaster\":true}," +
+        "\"owner\":{\"contactEmail\":\"mantisoss@netflix.com\",\"description\":\"\",\"name\":\"Mantis OSS\","
+        + "\"repo\":\"\",\"teamName\":\"\"}}";
+
+
+    private static final String QUICK_SUBMIT =
+        "{\"name\":\"hello-sine-testcontainers\",\"user\":\"mantisoss\",\"jobSla\":{\"durationType\":\"Perpetual\","
+            + "\"runtimeLimitSecs\":\"0\",\"minRuntimeSecs\":\"0\",\"userProvidedType\":\"\"}}";
 
 
     private static final OkHttpClient HTTP_CLIENT = new OkHttpClient();
@@ -95,8 +121,13 @@ public class TestContainerHelloWorld {
             final String agentId0 = "agent0";
             GenericContainer<?> agent0 = createAgent(agentId0, CLUSTER_ID, network);
 
-            if (!ensureAgentStarted(master.getHost(),
-                master.getMappedPort(CONTROL_PLANE_API_PORT),
+            String controlPlaneHost = master.getHost();
+
+            int controlPlanePort = master.getMappedPort(CONTROL_PLANE_API_PORT);
+
+            if (!ensureAgentStarted(
+                controlPlaneHost,
+                controlPlanePort,
                 CLUSTER_ID,
                 agentId0,
                 agent0,
@@ -105,7 +136,13 @@ public class TestContainerHelloWorld {
                     fail("Failed to register agent: " + agent0.getContainerId());
             }
 
+            createJobCluster(controlPlaneHost, controlPlanePort);
+            getJobCluster(controlPlaneHost, controlPlanePort);
+            quickSubmitJobCluster(controlPlaneHost, controlPlanePort);
 
+            // getJobSink(agent0);
+
+            log.warn("Waiting for exit test.");
             Thread.sleep(Duration.ofSeconds(360).toMillis());
         }
 
@@ -147,8 +184,7 @@ public class TestContainerHelloWorld {
         String agentId,
         GenericContainer<?> agent,
         int retries,
-        long sleepMillis)
-        throws InterruptedException, IOException {
+        long sleepMillis) {
         agent.start();
         log.info("{} agent started.", agent.getContainerId());
         HttpUrl reqUrl = new Builder()
@@ -187,5 +223,116 @@ public class TestContainerHelloWorld {
             }
         }
         return false;
+    }
+
+    private boolean createJobCluster(
+        String controlPlaneHost,
+        int controlPlanePort) {
+        HttpUrl reqUrl = new Builder()
+            .scheme("http")
+            .host(controlPlaneHost)
+            .port(controlPlanePort)
+            .addPathSegments("api/v1/jobClusters")
+            .build();
+        log.info("Req: {}", reqUrl);
+
+        RequestBody body = RequestBody.create(
+            JOB_CLUSTER_CREATE, MediaType.parse("application/json; charset=utf-8"));
+
+        Request request = new Request.Builder()
+            .url(reqUrl)
+            .post(body)
+            .build();
+
+        try {
+            Response response = HTTP_CLIENT.newCall(request).execute();
+            String responseBody = response.body().string();
+            log.info("Created job cluster response: {}.", responseBody);
+
+        } catch (Exception e) {
+            log.warn("Create cluster call error", e);
+        }
+        return true;
+    }
+
+    private boolean quickSubmitJobCluster(
+        String controlPlaneHost,
+        int controlPlanePort) {
+        HttpUrl reqUrl = new Builder()
+            .scheme("http")
+            .host(controlPlaneHost)
+            .port(controlPlanePort)
+            .addPathSegments("api/namedjob/quicksubmit")
+            .build();
+        log.info("Req: {}", reqUrl);
+
+        RequestBody body = RequestBody.create(
+            QUICK_SUBMIT, MediaType.parse("application/json; charset=utf-8"));
+
+        Request request = new Request.Builder()
+            .url(reqUrl)
+            .post(body)
+            .build();
+
+        try {
+            Response response = HTTP_CLIENT.newCall(request).execute();
+            String responseBody = response.body().string();
+            log.info("Quick submit job response: {}.", responseBody);
+
+        } catch (Exception e) {
+            log.warn("Quick submit job call error", e);
+        }
+        return true;
+    }
+
+    private boolean getJobCluster(
+        String controlPlaneHost,
+        int controlPlanePort) {
+        HttpUrl reqUrl = new Builder()
+            .scheme("http")
+            .host(controlPlaneHost)
+            .port(controlPlanePort)
+            .addPathSegments("api/v1/jobClusters")
+            .addPathSegment(JOB_CLUSTER_NAME)
+            .build();
+        log.info("Req: {}", reqUrl);
+
+        Request request = new Request.Builder()
+            .url(reqUrl)
+            .build();
+
+        try {
+            Response response = HTTP_CLIENT.newCall(request).execute();
+            String responseBody = response.body().string();
+            log.info("Get job cluster response: {}.", responseBody);
+
+        } catch (Exception e) {
+            log.warn("GET job cluster call error", e);
+        }
+        return true;
+    }
+
+    private boolean getJobSink(
+        GenericContainer<?> agent) {
+        HttpUrl reqUrl = new Builder()
+            .scheme("http")
+            .host(agent.getHost())
+            .port(agent.getMappedPort(5055))
+            .build();
+        log.info("Req: {}", reqUrl);
+
+        Request request = new Request.Builder()
+            .url(reqUrl)
+            .build();
+
+        try {
+            Response response = HTTP_CLIENT.newCall(request).execute();
+            String responseBody = response.body().string();
+            log.info("Get job sink response: {}.", responseBody);
+
+        } catch (Exception e) {
+            log.warn("GET job sink call error", e);
+        }
+        return true;
     }
 }
