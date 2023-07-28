296153ce391fc50ea25bb7f6e791c13fffe93820
Calvin Cheung <ccheung@netflix.com>
2023-07-28 11:16:06 -0700

    update comments

--- mantis-server/mantis-server-agent/buildDockerImage.sh
+++ mantis-server/mantis-server-agent/buildDockerImage.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# build the mantis-server-worker fat jar
+# build the mantis-server-agent fat jar
 ../../gradlew clean installDist
+
+# create sample job artifact to include in the image
 ../../gradlew :mantis-examples:mantis-examples-sine-function:mantisZipArtifact
 
 mkdir build/distributions
 cp ../../mantis-examples/mantis-examples-sine-function/build/distributions/* build/distributions/
 
-# build the Docker image that packages the mantis-server-worker along with a running mesos-slave
+# build the Docker image that packages the mantis-server-agent
 docker build -t netflixoss/mantisagent .
 
 echo "Created Docker image 'netflixoss/mantisagent'"
