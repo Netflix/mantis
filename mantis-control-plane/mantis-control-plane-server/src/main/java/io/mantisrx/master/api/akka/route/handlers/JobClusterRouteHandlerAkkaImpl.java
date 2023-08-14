546f4aa821a3279c748afb510d1da6777e96293c
hmitnflx <100323213+hmitnflx@users.noreply.github.com>
2023-08-14 14:43:03 -0700

    Make jobCluster API calls return latest job in launched state instead of latest submitted (#518)
    * namedjobs return the last job in launched state instead of submitted
    * even for discovery return latest job in launched state
    * review comment

--- mantis-control-plane/mantis-control-plane-server/src/main/java/io/mantisrx/master/api/akka/route/handlers/JobClusterRouteHandlerAkkaImpl.java
+++ mantis-control-plane/mantis-control-plane-server/src/main/java/io/mantisrx/master/api/akka/route/handlers/JobClusterRouteHandlerAkkaImpl.java
@@ -54,104 +54,90 @@ public class JobClusterRouteHandlerAkkaImpl implements JobClusterRouteHandler {
 
     @Override
     public CompletionStage<JobClusterManagerProto.CreateJobClusterResponse> create(final JobClusterManagerProto.CreateJobClusterRequest request) {
-        CompletionStage<JobClusterManagerProto.CreateJobClusterResponse> response = ask(jobClustersManagerActor, request, timeout)
+        return ask(jobClustersManagerActor, request, timeout)
                 .thenApply(JobClusterManagerProto.CreateJobClusterResponse.class::cast);
-        return response;
     }
 
     @Override
     public CompletionStage<JobClusterManagerProto.UpdateJobClusterResponse> update(JobClusterManagerProto.UpdateJobClusterRequest request) {
-        CompletionStage<JobClusterManagerProto.UpdateJobClusterResponse> response = ask(jobClustersManagerActor, request, timeout)
+        return ask(jobClustersManagerActor, request, timeout)
                 .thenApply(JobClusterManagerProto.UpdateJobClusterResponse.class::cast);
-        return response;
     }
 
     @Override
     public CompletionStage<JobClusterManagerProto.DeleteJobClusterResponse> delete(JobClusterManagerProto.DeleteJobClusterRequest request) {
-        CompletionStage<JobClusterManagerProto.DeleteJobClusterResponse> response = ask(jobClustersManagerActor, request, timeout)
+        return ask(jobClustersManagerActor, request, timeout)
             .thenApply(JobClusterManagerProto.DeleteJobClusterResponse.class::cast);
-        return response;
     }
 
     @Override
     public CompletionStage<JobClusterManagerProto.DisableJobClusterResponse> disable(JobClusterManagerProto.DisableJobClusterRequest request) {
-        CompletionStage<JobClusterManagerProto.DisableJobClusterResponse> response = ask(jobClustersManagerActor, request, timeout)
+        return ask(jobClustersManagerActor, request, timeout)
             .thenApply(JobClusterManagerProto.DisableJobClusterResponse.class::cast);
-        return response;
     }
 
     @Override
     public CompletionStage<JobClusterManagerProto.EnableJobClusterResponse> enable(JobClusterManagerProto.EnableJobClusterRequest request) {
-        CompletionStage<JobClusterManagerProto.EnableJobClusterResponse> response = ask(jobClustersManagerActor, request, timeout)
+        return ask(jobClustersManagerActor, request, timeout)
             .thenApply(JobClusterManagerProto.EnableJobClusterResponse.class::cast);
-        return response;
     }
 
     @Override
     public CompletionStage<JobClusterManagerProto.UpdateJobClusterArtifactResponse> updateArtifact(JobClusterManagerProto.UpdateJobClusterArtifactRequest request) {
-        CompletionStage<JobClusterManagerProto.UpdateJobClusterArtifactResponse> response = ask(jobClustersManagerActor, request, timeout)
+        return ask(jobClustersManagerActor, request, timeout)
             .thenApply(JobClusterManagerProto.UpdateJobClusterArtifactResponse.class::cast);
-        return response;
     }
 
     @Override
     public CompletionStage<UpdateSchedulingInfoResponse> updateSchedulingInfo(String clusterName, UpdateSchedulingInfoRequest request) {
-        CompletionStage<UpdateSchedulingInfoResponse> response =
-            ask(
-                jobClustersManagerActor,
-                new UpdateSchedulingInfo(request.requestId, clusterName, request.getSchedulingInfo(),
-                    request.getVersion()),
-                timeout)
-            .thenApply(UpdateSchedulingInfoResponse.class::cast);
-        return response;
+        return ask(
+            jobClustersManagerActor,
+            new UpdateSchedulingInfo(request.requestId, clusterName, request.getSchedulingInfo(),
+                request.getVersion()),
+            timeout)
+        .thenApply(UpdateSchedulingInfoResponse.class::cast);
     }
 
     @Override
     public CompletionStage<JobClusterManagerProto.UpdateJobClusterSLAResponse> updateSLA(JobClusterManagerProto.UpdateJobClusterSLARequest request) {
-        CompletionStage<JobClusterManagerProto.UpdateJobClusterSLAResponse> response = ask(jobClustersManagerActor, request, timeout)
+        return ask(jobClustersManagerActor, request, timeout)
             .thenApply(JobClusterManagerProto.UpdateJobClusterSLAResponse.class::cast);
-        return response;
     }
 
     @Override
     public CompletionStage<JobClusterManagerProto.UpdateJobClusterWorkerMigrationStrategyResponse> updateWorkerMigrateStrategy(JobClusterManagerProto.UpdateJobClusterWorkerMigrationStrategyRequest request) {
-        CompletionStage<JobClusterManagerProto.UpdateJobClusterWorkerMigrationStrategyResponse> response = ask(jobClustersManagerActor, request, timeout)
+        return ask(jobClustersManagerActor, request, timeout)
             .thenApply(JobClusterManagerProto.UpdateJobClusterWorkerMigrationStrategyResponse.class::cast);
-        return response;
     }
 
     @Override
     public CompletionStage<JobClusterManagerProto.UpdateJobClusterLabelsResponse> updateLabels(JobClusterManagerProto.UpdateJobClusterLabelsRequest request) {
-        CompletionStage<JobClusterManagerProto.UpdateJobClusterLabelsResponse> response = ask(jobClustersManagerActor, request, timeout)
+        return ask(jobClustersManagerActor, request, timeout)
             .thenApply(JobClusterManagerProto.UpdateJobClusterLabelsResponse.class::cast);
-        return response;
     }
 
     @Override
     public CompletionStage<JobClusterManagerProto.SubmitJobResponse> submit(JobClusterManagerProto.SubmitJobRequest request) {
-        CompletionStage<JobClusterManagerProto.SubmitJobResponse> response = ask(jobClustersManagerActor, request, timeout)
+        return ask(jobClustersManagerActor, request, timeout)
             .thenApply(JobClusterManagerProto.SubmitJobResponse.class::cast);
-        return response;
     }
 
     @Override
     public CompletionStage<JobClusterManagerProto.GetJobClusterResponse> getJobClusterDetails(JobClusterManagerProto.GetJobClusterRequest request) {
-        CompletionStage<JobClusterManagerProto.GetJobClusterResponse> response = ask(jobClustersManagerActor, request, timeout)
+        return ask(jobClustersManagerActor, request, timeout)
             .thenApply(JobClusterManagerProto.GetJobClusterResponse.class::cast);
-        return response;
     }
 
     @Override
     public CompletionStage<JobClusterManagerProto.ListJobClustersResponse> getAllJobClusters(JobClusterManagerProto.ListJobClustersRequest request) {
         allJobClustersGET.increment();
-        CompletionStage<JobClusterManagerProto.ListJobClustersResponse> response = ask(jobClustersManagerActor, request, timeout)
+        return ask(jobClustersManagerActor, request, timeout)
             .thenApply(JobClusterManagerProto.ListJobClustersResponse.class::cast);
-        return response;
     }
 
     @Override
     public CompletionStage<JobClusterManagerProto.GetLatestJobDiscoveryInfoResponse> getLatestJobDiscoveryInfo(JobClusterManagerProto.GetLatestJobDiscoveryInfoRequest request) {
-        CompletionStage<JobClusterManagerProto.GetLatestJobDiscoveryInfoResponse> response = ask(jobClustersManagerActor, request, timeout)
+        return ask(jobClustersManagerActor, request, timeout)
             .thenApply(JobClusterManagerProto.GetLatestJobDiscoveryInfoResponse.class::cast);
-        return response;    }
+    }
 }
