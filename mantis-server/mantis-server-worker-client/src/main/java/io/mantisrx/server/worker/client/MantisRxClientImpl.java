cdf59046c47702d94f907aa56d5022bdda27c9bc
kriti-sc <kathuriakriti1@gmail.com>
2023-08-22 12:58:57 -0400

    add copyright info, fix module import

--- mantis-server/mantis-server-worker-client/src/main/java/io/mantisrx/server/worker/client/MantisRxClientImpl.java
+++ mantis-server/mantis-server-worker-client/src/main/java/io/mantisrx/server/worker/client/MantisRxClientImpl.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.mantisrx.server.worker.client;
 
 import io.netty.bootstrap.Bootstrap;
@@ -16,9 +32,9 @@ import mantis.io.reactivex.netty.metrics.MetricEventsSubject;
 import mantis.io.reactivex.netty.pipeline.PipelineConfigurator;
 import mantis.io.reactivex.netty.pipeline.PipelineConfigurators;
 import rx.Observable;
+import rx.Observable.OnSubscribe;
 import rx.Subscriber;
 import rx.Subscription;
-import rx.Observable.OnSubscribe;
 
 // TODO: Delete this class
 
