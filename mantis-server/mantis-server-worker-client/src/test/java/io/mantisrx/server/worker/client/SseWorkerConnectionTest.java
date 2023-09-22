a8d27a7e971af091ae797284452debc4bf9e16e8
kriti-sc <kathuriakriti1@gmail.com>
2023-09-22 13:09:11 -0400

    remove unused libraries

--- mantis-server/mantis-server-worker-client/src/test/java/io/mantisrx/server/worker/client/SseWorkerConnectionTest.java
+++ mantis-server/mantis-server-worker-client/src/test/java/io/mantisrx/server/worker/client/SseWorkerConnectionTest.java
@@ -17,7 +17,6 @@
 package io.mantisrx.server.worker.client;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
