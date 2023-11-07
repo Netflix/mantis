af248c5d7adb74778f502c9272e108e006a4ecd3
Sundaram Ananthanarayanan <sananthanarayanan@netflix.com>
2023-11-06 16:20:29 -0800

    Spotless bugs

--- mantis-server/mantis-server-agent/src/test/java/io/mantisrx/server/agent/utils/DurableBooleanStateTest.java
+++ mantis-server/mantis-server-agent/src/test/java/io/mantisrx/server/agent/utils/DurableBooleanStateTest.java
@@ -17,11 +17,9 @@
 package io.mantisrx.server.agent.utils;
 
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
-import java.net.URL;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
