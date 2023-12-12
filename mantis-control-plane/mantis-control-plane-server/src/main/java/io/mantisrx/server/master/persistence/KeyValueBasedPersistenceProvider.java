7118ee8a7aecad439688574d27691e0e15165ef8
Sundaram Ananthanarayanan <sananthanarayanan@netflix.com>
2023-12-11 20:57:53 -0800

    Formatting

--- mantis-control-plane/mantis-control-plane-server/src/main/java/io/mantisrx/server/master/persistence/KeyValueBasedPersistenceProvider.java
+++ mantis-control-plane/mantis-control-plane-server/src/main/java/io/mantisrx/server/master/persistence/KeyValueBasedPersistenceProvider.java
@@ -58,6 +58,7 @@ import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -583,7 +584,8 @@ public class KeyValueBasedPersistenceProvider implements IMantisPersistenceProvi
                     }
                 })
             .map(DataFormatAdapter::convertNamedJobCompletedJobToCompletedJob)
-            .sorted(Comparator.<CompletedJob>comparingLong(job -> JobId.fromId(job.getJobId()).get().getJobNum()).reversed())
+            .sorted(Comparator.<CompletedJob>comparingLong(
+                job -> JobId.fromId(job.getJobId()).get().getJobNum()).reversed())
             .collect(Collectors.toList());
     }
 
