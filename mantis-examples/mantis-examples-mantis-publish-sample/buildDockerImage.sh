842c9fa857711e128e056a8a45775f2ae977056d
Sundaram Ananthanarayanan <sananthanarayanan@netflix.com>
2024-01-12 07:46:48 -0800

    Docker image for Sample Data Publisher

--- mantis-examples/mantis-examples-mantis-publish-sample/buildDockerImage.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# build the mantis-server-worker fat jar
-../gradlew clean build shadowDistZip
-unzip build/distributions/mantis-examples-mantis-publish-sample-shadow-0.1.0-SNAPSHOT.zip -d build/distributions 
-# build the Docker image that packages the mantis-server-worker along with a running mesos-slave
-docker build -t dev/mantispublish .
-
-echo "Created Docker image 'dev/mantispublish'"
