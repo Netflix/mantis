1a95ae5762b9374f591aad218a64be7ad780911c
Sundaram Ananthanarayanan <sananthanarayanan@netflix.com>
2024-01-18 09:42:31 -0800

    Getting rid of vagrant since it's not being used and not maintained; Move to helm

--- localdev/createAndSubmit.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-curl -X POST http://127.0.0.1:8100/api/namedjob/create --silent --data @conf/namedJob-templateWithAdaptiveScaling -vvv
-sleep 5 
-curl -X POST http://127.0.0.1:8100/api/submit --silent --data @conf/submitJob-templateWithAdaptiveScaling -vvv
