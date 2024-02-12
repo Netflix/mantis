4d7b231a0dddf97b8effd40e215aead01d589e66
Sundaram Ananthanarayanan <sananthanarayanan@netflix.com>
2024-02-12 14:44:41 -0800

    Adding more logs to SubscriptionStateHandler

--- mantis-runtime-loader/src/main/java/io/mantisrx/runtime/loader/SubscriptionStateHandlerImpl.java
+++ mantis-runtime-loader/src/main/java/io/mantisrx/runtime/loader/SubscriptionStateHandlerImpl.java
@@ -66,16 +66,20 @@ class SubscriptionStateHandlerImpl extends AbstractScheduledService implements S
     @Override
     protected void runOneIteration() {
         SubscriptionState state = currentState.get();
-        if (state.isUnsubscribedFor(subscriptionTimeout) && state.hasRunFor(minRuntime)) {
-            try {
-                log.info("Calling master to kill due to subscription timeout");
-                masterClientApi.killJob(jobId, "MantisWorker", "No subscriptions for " +
-                                subscriptionTimeout.getSeconds() + " secs")
+        if (state.isUnsubscribedFor(subscriptionTimeout)) {
+            if (state.hasRunFor(minRuntime)) {
+                try {
+                    log.info("Calling master to kill due to subscription timeout");
+                    masterClientApi.killJob(jobId, "MantisWorker", "No subscriptions for " +
+                            subscriptionTimeout.getSeconds() + " secs")
                         .single()
                         .toBlocking()
                         .first();
-            } catch (Exception e) {
-                log.error("Failed to kill job {} due to no subscribers for {} seconds", jobId, state.getUnsubscribedDuration().getSeconds());
+                } catch (Exception e) {
+                    log.error("Failed to kill job {} due to no subscribers for {} seconds", jobId, state.getUnsubscribedDuration().getSeconds());
+                }
+            } else {
+                log.info("Not killing job {} as it has not run for minRuntimeSecs {}", jobId, minRuntime.getSeconds());
             }
         }
     }
@@ -86,11 +90,13 @@ class SubscriptionStateHandlerImpl extends AbstractScheduledService implements S
             log.error("currentState in SubscriptionStateHandlerImpl is not set onSinkUnsubscribed");
             return;
         }
+        log.info("Sink unsubscribed");
         currentState.updateAndGet(SubscriptionState::onSinkUnsubscribed);
     }
 
     @Override
     public void onSinkSubscribed() {
+        log.info("Sink subscribed");
         Preconditions.checkNotNull(currentState.get(), "currentState is not intialized onSinkSubscribed.");
         currentState.updateAndGet(SubscriptionState::onSinkSubscribed);
     }
