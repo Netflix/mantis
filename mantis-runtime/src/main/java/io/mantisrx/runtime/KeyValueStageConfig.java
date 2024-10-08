38c8d4f92d00bb85c98ee683143e6c13cc2e5c49
Cody Rioux <crioux@stripe.com>
2024-10-07 17:04:21 -0700

    Call correct peer constructor in KeyValueStageConfig instead of super

--- mantis-runtime/src/main/java/io/mantisrx/runtime/KeyValueStageConfig.java
+++ mantis-runtime/src/main/java/io/mantisrx/runtime/KeyValueStageConfig.java
@@ -33,8 +33,7 @@ public abstract class KeyValueStageConfig<T, K, R> extends StageConfig<T, R> {
     private final Codec<K> keyCodec;
 
     public KeyValueStageConfig(String description, Codec<?> inputKeyCodec, Codec<T> inputCodec, Codec<K> outputKeyCodec, Codec<R> outputCodec, INPUT_STRATEGY inputStrategy, List<ParameterDefinition<?>> params) {
-        super(description, inputKeyCodec, inputCodec, outputCodec, inputStrategy, params, DEFAULT_STAGE_CONCURRENCY);
-        this.keyCodec = outputKeyCodec;
+        this(description, inputKeyCodec, inputCodec, outputKeyCodec, outputCodec, inputStrategy, params, DEFAULT_STAGE_CONCURRENCY);
     }
 
     public KeyValueStageConfig(String description, Codec<?> inputKeyCodec, Codec<T> inputCodec, Codec<K> outputKeyCodec, Codec<R> outputCodec, INPUT_STRATEGY inputStrategy, List<ParameterDefinition<?>> params, int concurrency) {
